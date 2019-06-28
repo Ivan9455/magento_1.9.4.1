8044a5735af68b90952fe89be70fabc08c9b66e5
Ivan Paderin <paderin94126@gmail.com>
2019-06-28 17:35:00 +0600

    сделал вывод записей из bd

--- src/app/code/local/CodeDrop/CallRequest/Block/List.php
+++ src/app/code/local/CodeDrop/CallRequest/Block/List.php
@@ -3,9 +3,11 @@
 class CodeDrop_CallRequest_Block_List extends Mage_Core_Block_Template
 {
 
-//    public function getBlocks()
-//    {
-//        return Mage::getModel('callrequest/block')->getCollection()->addFieldToFilter('id',
+    public function getBlocks()
+    {
+
+        return Mage::getModel('codedrop_callrequest/phone')->getCollection();
+//        return Mage::getModel('callrequest/phone')->getCollection()->addFieldToFilter('id',
 //            ['eq' => CodeDrop_CallRequest_Model_Source_Status::ENABLED]);
-//    }
+    }
 }
